d747cdf19c2b4e6ef861d1c32ae1ba0887173385
Brian Pendleton <bpendleton@numeric.com>
2015-04-10 12:11:23 -0400

    Speaking in tongues was a tricky solution because it required using the actual instructions from the case as the static character map in the code. They didn't provide a mapping for the letter q, which you had to look through the output and figure out that is was mapped to z.

---
+++ src/cj2012/qualification/tongues.hs
@@ -0,0 +1,49 @@
+import Data.List
+import Control.Monad
+import Data.Function
+import qualified Data.Map as Map
+import qualified Data.Maybe as May
+
+
+type Case = String
+type Solution = String
+
+-- This map is constructed from the cases instructions
+-- When running this the first time, it crashed because the "q" -> "z" mapping didn't exist
+charMap = Map.fromList (zip bogus correct)
+	where
+		bogus = "qaozejp mysljylc kd kxveddknmc re jsicpdrysi" ++ "rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd" ++ "de kr kd eoya kw aej tysr re ujdr lkgc jv" 
+		correct = "zyeqour language is impossible to understand" ++ "there are twenty six factorial possibilities" ++ "so it is okay if you want to just give up"
+
+
+--solve :: Case -> Solution
+solve x = [May.fromJust (Map.lookup c charMap) | c <- x]
+
+
+formatSolution :: Solution -> String
+formatSolution sol = sol
+
+
+readCase :: IO Case
+readCase = do
+	line <- getLine
+	return line
+
+
+readCases :: IO [Case]
+readCases = do
+	n <- getLine
+	res <- replicateM (read n) readCase
+	--res <- replicateM 1 readCase
+	return res
+
+
+main = do
+	cases <- readCases
+	let	
+		solutions = map solve cases
+		formattedSolutions = map formatSolution solutions
+		output = zipWith (++) prefixes formattedSolutions
+	putStr $ unlines output
+
+prefixes = [ "Case #" ++ show n ++ ": " | n <- [1..]]
